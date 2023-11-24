dc2e5db5812a45b9a85a015e4d17546d84520bf9
omoniyiayomide987@gmail.com <omoniyiayomide987@gmail.com>
2023-11-24 08:48:00 +0100

    first commit

---
+++ Arithmetic.py
@@ -0,0 +1,15 @@
+express=27.5
+oper=2
+
+sum=express+oper
+print(sum)
+subtract=express-oper
+print(subtract)
+mult=express*oper
+print(mult)
+divid=express/oper
+print(divid)
+double=express//oper
+print(double)
+doublex=express**oper
+print(doublex)
